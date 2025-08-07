1d033e446b25ec02f9c7624b3faf08b39ad5b169
savindi001 <savindihashini357@gmail.com>
2025-08-07 12:49:24 +0530

    Add files via upload

--- pro5.py
+++ pro5.py
@@ -17,44 +17,36 @@ with col1:
         bsalary=int(bsalary)
 
 
-   #Calculation part
     if st.button("Calculate"):
-        try:
-            row=[]
+        row = []
 
-            if jobp == "Manager":
-                bonus = bsalary * 20 / 100
-                
+        if jobp == "Manager":
+            bonus = bsalary * 20 / 100
+        elif jobp == "Clark":
+            bonus = bsalary * 10 / 100
+        else:  # Other
+            bonus = bsalary * 5 / 100
 
-            if jobp == "Clark":
-                bonus = bsalary * 10 / 100
-                
+        total_salary = bsalary + bonus
 
-            if jobp == "Other":
-                bonus = bsalary * 5 / 100
+        row.append([jobp, bsalary, bonus, total_salary])
                 
             
-            bonus=bonus
-            Total_salary=bsalary+bonus
-            row.append(["Manager",bsalary,bonus,Total_salary])
-            row.append(["Clark",bsalary,bonus,Total_salary])
-            row.append(["Other",bsalary,bonus,Total_salary])
-
-            Total_salary=round(bsalary+bonus,2)
             
 
-            st.info(f"Total Salary: Rs.{Total_salary:.2f}")
+    Total_salary=round(bsalary+bonus,2)
+            
+
+    st.info(f"Total Salary: Rs.{Total_salary:.2f}")
 
              #convert to dataframe and show table
-            df=pd.DataFrame(row,columns=["Description","Basic Salary","Bonus","Total Salary"])
+    df=pd.DataFrame(row,columns=["Description","Basic Salary","Bonus","Total Salary"])
 
                 #format the rate and cost columns
-            df["Bonus"] = df["Bonus"].apply(lambda x: f"{x:.2f}" if isinstance(x,(int,float))else x)
-            df["Total Salary"] = df["Total Salary"].apply(lambda x: f"{x:.2f}" if isinstance(x,(int,float))else x)
-            st.table(df)
+    df["Bonus"] = df["Bonus"].apply(lambda x: f"{x:.2f}" if isinstance(x,(int,float))else x)
+    df["Total Salary"] = df["Total Salary"].apply(lambda x: f"{x:.2f}" if isinstance(x,(int,float))else x)
+    st.table(df)
 
-        except Exception as e:
-            st.error(f"Error calculating bill:{e}")
 
 with col2:
     	st.text_input("Options\nManager\nClark\nOther")
